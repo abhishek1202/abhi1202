7791e1a3176b7c550b65b28eed572412a0a1546f
Abhishek Anand <anandabhishek1202@gmail.com>
2020-07-05 15:42:33 +0530

    full Crud API in Products and Customer

--- src/main/java/com/Ecommercial/Portal/Rest/CustomerRestCantroller.java
+++ src/main/java/com/Ecommercial/Portal/Rest/CustomerRestCantroller.java
@@ -3,14 +3,12 @@ package com.Ecommercial.Portal.Rest;
 import com.Ecommercial.Portal.Entity.Customer;
 import com.Ecommercial.Portal.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api1")
+@RequestMapping("/api")
 public class CustomerRestCantroller {
 
     private CustomerService customerService;
@@ -23,4 +21,44 @@ public class CustomerRestCantroller {
     @GetMapping("/customer")
     public List<Customer> findall() {
         return customerService.findAll();   }
+
+        @GetMapping("/customer/{customerId}")
+    public Customer findCustomerById(@PathVariable int customerId)
+        {
+            Customer customer=customerService.findById(customerId);
+            if (customer==null)
+            {
+                throw new RuntimeException("Customer is not available"+customerId);
+            }
+            return customer;
+        }
+
+        @PostMapping("/customer")
+        public Customer addCustomer(@RequestBody Customer customer)
+        {
+            customer.setId(0);
+            customerService.save(customer);
+            return customer;
+        }
+
+        @PutMapping("/customer")
+    public Customer updateCustomer(@RequestBody Customer customer)
+        {
+            customerService.save(customer);
+            return customer;
+        }
+
+        @DeleteMapping("/customer/{customerId}")
+    public String deletedCustomerById(int customerId)
+        {
+            Customer customer=customerService.findById(customerId);
+            if (customer==null)
+            {
+                throw new RuntimeException("Customer is not available"+customerId);
+            }
+            customerService.deleteById(customerId);
+            return "Deleetd";
+        }
+
+
 }
--- src/main/java/com/Ecommercial/Portal/Rest/ProductRestCantroller.java
+++ src/main/java/com/Ecommercial/Portal/Rest/ProductRestCantroller.java
@@ -3,10 +3,7 @@ package com.Ecommercial.Portal.Rest;
 import com.Ecommercial.Portal.Entity.Products;
 import com.Ecommercial.Portal.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,7 +24,46 @@ public class ProductRestCantroller {
         return productService.findAll();
     }
 
-    @GetMapping("/rating//{id}")
+    @GetMapping("/products/{theId}")
+    public Products findById(@PathVariable int theId)
+    {
+        Products products=productService.findById(theId);
+
+        if (products==null)
+        {
+            throw new RuntimeException("Production id is not found Error"+theId);
+        }
+
+        return products;
+    }
+
+    @PostMapping("/products")
+    public Products addProducts(@RequestBody Products products){
+        products.setId(0);
+        productService.save(products);
+        return products;
+    }
+
+    @PutMapping("/products")
+    public Products updateProducts(@RequestBody Products products)
+    {
+        productService.save(products);
+        return products;
+    }
+
+    @DeleteMapping("/products/{theId}")
+    public String deleteById(@PathVariable int theId)
+    {
+        Products products=productService.findById(theId);
+        if (products==null)
+        {
+            throw new RuntimeException("Product id not available"+ theId);
+        }
+        productService.deleteById(theId);
+        return "Deleted";
+    }
+
+    @GetMapping("/rating/{id}")
     public List<Products> findByRating(@PathVariable int id)
     {
         return productService.findByRating(id);
